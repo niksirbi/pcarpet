228b36786596cee8fdcfd6142ef6020a756d7683
niksirbi <niko.sirbiladze@gmail.com>
2021-09-30 16:58:08 +0200

    interpolation truned off for matplotlib imshow

--- pcarpet/pcarpet.py
+++ pcarpet/pcarpet.py
@@ -411,8 +411,9 @@ class Dataset(object):
         # Carpet plot
         ax1 = plt.subplot2grid((6 + npc, 5), (0, 0),
                                rowspan=5, colspan=3)
-        carpet_plot = ax1.imshow(self.carpet, aspect='auto',
-                                 cmap='Greys_r', vmin=-2, vmax=2)
+        carpet_plot = ax1.imshow(self.carpet, interpolation='none',
+                                 aspect='auto', cmap='Greys_r',
+                                 vmin=-2, vmax=2)
         ax1.set_xlabel(f'Time ({self.carpet.shape[1]} TRs)')
         ax1.set_ylabel(f'Space ({self.carpet.shape[0]} voxels)')
         ax1.set_xticks([])
@@ -456,8 +457,9 @@ class Dataset(object):
 
         # Plot fPC-carpet correlations as matrix
         ax3 = plt.subplot2grid((6 + npc, 5), (0, 3), rowspan=5, colspan=1)
-        R_matrix = ax3.imshow(self.fPC_carpet_R, aspect='auto',
-                              cmap='coolwarm', vmin=-1, vmax=1)
+        R_matrix = ax3.imshow(self.fPC_carpet_R, interpolation='none',
+                              aspect='auto', cmap='coolwarm',
+                              vmin=-1, vmax=1)
         ax3.set_xticks(np.arange(npc))
         ax3.set_xticklabels(np.arange(1, npc + 1))
         ax3.set_yticks([])
